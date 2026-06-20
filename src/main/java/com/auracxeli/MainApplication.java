a603354e66264116f4a162e44fff95fef0fd2865
Heisenberg976 <nikamiqeltadze159@gmail.com>
2026-06-20 15:33:29 +0400

    dropped SPRING_JPA_HIBERNATE_DDL_AUTO: update now that Flyway owns schema

--- src/main/java/com/auracxeli/MainApplication.java
+++ src/main/java/com/auracxeli/MainApplication.java
@@ -2,10 +2,8 @@ package com.auracxeli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
