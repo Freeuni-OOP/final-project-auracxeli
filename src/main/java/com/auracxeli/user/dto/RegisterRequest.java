73c7143535ed05c79c0663ba75d3ec52e41a40d1
ZukaCS <zukakekelidze4@gmail.com>
2026-06-21 17:27:25 +0400

    feat: add validation annotations to RegisterRequest dto

--- src/main/java/com/auracxeli/user/dto/RegisterRequest.java
+++ src/main/java/com/auracxeli/user/dto/RegisterRequest.java
@@ -1,8 +1,18 @@
 package com.auracxeli.user.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest (
-    String username,
-    String email,
-    String password
+        @NotBlank(message = "მომხმარებლის სახელი სავალდებულოა")
+        @Size(min = 5, max = 50, message = "სახელის ზომა უნდა იყოს 5 დან 50 სიმბოლომდე")
+        String username,
+        @NotBlank(message = "ელფოსტა სავალდებულოა")
+        @Email(message = "ელფოსტის ფორმატი არასწორია")
+        @Size(max = 100, message = "ელფოსტა ძალიან გრძელია")
+        String email,
+        @NotBlank(message = "პაროლი სავალდებულოა")
+        @Size(min = 8, message = "პაროლი მინიმუმ 8 სიმბოლო უნდა იყოს")
+        String password
 ){ }
