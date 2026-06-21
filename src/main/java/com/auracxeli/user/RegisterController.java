8b04985c138570e331d22071d4352e89646d8a17
ZukaCS <zukakekelidze4@gmail.com>
2026-06-21 18:08:19 +0400

    feat: add RegisterController and postMapping

---
+++ src/main/java/com/auracxeli/user/RegisterController.java
@@ -0,0 +1,39 @@
+package com.auracxeli.user;
+
+import com.auracxeli.user.dto.RegisterRequest;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.validation.BindingResult;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class RegisterController {
+    private final UserService userService;
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute RegisterRequest registerRequest,
+                           BindingResult bindingResult) {
+        //ვალიდაციას თუ ვერ გაივლის თავიდან ცდის.
+        if (bindingResult.hasErrors()) {
+            return "register";
+        }
+        try {
+            userService.register(registerRequest);
+        } catch (DuplicateUserException e) {
+            //username an emaili dakavebuli tua
+            bindingResult.rejectValue(e.getField(), "duplicate", e.getMessage());
+            return "register";
+        }
+
+        return "redirect:/login";
+    }
+}
