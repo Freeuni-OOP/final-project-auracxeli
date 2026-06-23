28b9a8b5b9dde9e6b49f19da87b04e017e0f9cbd
Heisenberg976 <nikamiqeltadze159@gmail.com>
2026-06-23 22:02:53 +0400

    test:enable MockMvc and Spring Security test support

--- src/test/java/com/auracxeli/AbstractIntegrationTest.java
+++ src/test/java/com/auracxeli/AbstractIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.auracxeli;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -10,6 +11,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
 
