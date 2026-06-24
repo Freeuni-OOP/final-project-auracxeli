49401a6006c2300a02e5d458201b8f54e4a36f39
Aleksandre Bughadze <abugh23@freeuni.edu.ge>
2026-06-24 15:26:11 +0400

    fix: use UTC_TIMESTAMP instead of NOW for admin seed

--- src/main/resources/db/migration/V6__seed_admin_user.sql
+++ src/main/resources/db/migration/V6__seed_admin_user.sql
@@ -1,4 +1,6 @@
 -- V6__seed_admin_user.sql
 -- I added the migration for default admin account here the password is admin123, already hashed btw
 INSERT INTO users (username, email, password, role, created_at, is_active)
-VALUES ('admin', 'admin@kartuli-games.local', '$2b$10$cATNVF/8UIyAFo.IbUkD6.H4j57ifU2zNopyB/8brYVmXcsGAqprC', 'ADMIN', NOW(), true);
+VALUES
+    ('admin', 'admin@gmail.com', '$2b$10$vXIG.VYdfPsb4O0b2ytnOexZ6iOERDVJNS1vN5I/4Nyqnr9L9mADm', 'ADMIN', UTC_TIMESTAMP(), true),
+    ('admin1', 'admin1@gmail.com', '$2b$10$Vi1SS/FIPTKFxFnT3eDJBelhSfuXAG/zXM/.RmFwuScNxuJ25IlrG', 'ADMIN', UTC_TIMESTAMP(), true);
