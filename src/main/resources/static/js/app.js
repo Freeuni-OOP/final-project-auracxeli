bad05ba4a273322a7949314e5bb6cf653562abc0
lleba23-cmd <lleba23@freeuni.edu.ge>
2026-06-25 20:41:49 +0400

    fixed some typo and corrected message output on display

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,36 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Frontend JS loaded successfully!");
 
-       const geoUniCodes = /^[\u10D0-\u10FF]+$/;
-       const input = document.querySelector(".wordle-form input[type='text']");
-       const takeFromForm = document.querySelector(".wordle-form");
-       const error = document.getElementByid("js-error");
-       if(!input || !takeFromForm)return;
+    const georgianOnly = /^[\u10D0-\u10FF]+$/;
+    const georgianChar = /[\u10D0-\u10FF]/;
+    const input = document.querySelector(".wordle-form input[type='text']");
+    const takeFromForm = document.querySelector(".wordle-form");
+    const error = document.getElementById("js-error");
 
-       input.addEventListener("input", () => {
+    if (!input || !takeFromForm) return;
+
+    input.addEventListener("input", () => {
         const value = input.value;
-        const filtered = value.split("").filter(ch => geoUniCodes.test(ch)).join("");
+        const filtered = value.split("").filter(ch => georgianChar.test(ch)).join("");
         input.value = filtered;
-           if (value !== filtered) {
-            errorEl.textContent = "მხოლოდ ქართული ასოებია დაშვებული";
-            errorEl.style.display = "block";
-        } else {
-            errorEl.textContent = "";
-            errorEl.style.display = "none";
-        }
-       });
 
-       takeFromForm.addEventListener("submit", (event) => {
+        if (value !== filtered) {
+            error.textContent = "მხოლოდ ქართული ასოებია დაშვებული";
+            error.style.display = "block";
+        } else {
+            error.textContent = "";
+            error.style.display = "none";
+        }
+    });
+
+    takeFromForm.addEventListener("submit", (event) => {
         const value = input.value.trim();
-        if(!geoUniCodes.test(value)){
+
+        if (!georgianOnly.test(value)) {
             event.preventDefault();
-            
             error.textContent = "დაშვებულია მხოლოდ ქართული ასოები";
             error.style.display = "block";
             return;
         }
-         
-             error.textContent = "";
-            error.style.display = "none";
-       });
+
+        error.textContent = "";
+        error.style.display = "none";
+    });
 });
