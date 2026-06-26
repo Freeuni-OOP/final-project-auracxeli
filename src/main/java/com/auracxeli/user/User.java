60c4298fc08c1c3ff9ebea3bc7bd3363c8cc2643
Aleksandre Bughadze <abugh23@freeuni.edu.ge>
2026-06-26 18:43:05 +0400

    Chore: Lombok Refactor

--- src/main/java/com/auracxeli/user/User.java
+++ src/main/java/com/auracxeli/user/User.java
@@ -2,9 +2,13 @@ package com.auracxeli.user;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
