0a028a9a2599f46bf781202b499310f2bee8c8df
Aleksandre Bughadze <abugh23@freeuni.edu.ge>
2026-06-26 18:44:16 +0400

    Chore: Lombok Refactor

--- src/main/java/com/auracxeli/user/User.java
+++ src/main/java/com/auracxeli/user/User.java
@@ -51,14 +51,14 @@ public class User {
         }
     }
 
-    public Long getId()                 { return id; }
+   /* public Long getId()                 { return id; }
     public String getUsername()         { return username; }
     public String getEmail()            { return email; }
     public String getPassword()         { return password; }
     public Role getRole()               { return role; }
     public LocalDateTime getCreatedAt() { return createdAt; }
     public boolean isActive()           { return isActive; }
-
+    */
     public void setUsername(String username) { this.username = username; }
     public void setEmail(String email)       { this.email = email; }
     public void setPassword(String password) { this.password = password; }
