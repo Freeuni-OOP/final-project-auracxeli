b7d12d3b51a6af462c69f80ba05d7c4e39258885
Aleksandre Bughadze <abugh23@freeuni.edu.ge>
2026-06-28 11:04:55 +0400

    Feature: Connections entities and repos (Created and filled the repositories)

---
+++ src/main/java/com/auracxeli/connections/ConnectionsGroupRepository.java
@@ -0,0 +1,6 @@
+package com.auracxeli.connections;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConnectionsGroupRepository extends JpaRepository<ConnectionsGroup, Long> {
+}
