1ca9f600ae0699267b468175013566f485dfb501
Aleksandre Bughadze <abugh23@freeuni.edu.ge>
2026-07-09 15:39:45 +0400

    feat: fixes

--- src/main/java/com/auracxeli/user/ProfileController.java
+++ src/main/java/com/auracxeli/user/ProfileController.java
@@ -16,12 +16,16 @@ public class ProfileController {
     @GetMapping("/profile")
     public String showOwnProfile(@AuthenticationPrincipal UserDetailsImpl currentUser, Model model) {
         model.addAttribute("profile", profileService.getProfile(currentUser.getUsername()));
+        model.addAttribute("isOwnProfile", true);
         return "profile";
     }
 
     @GetMapping("/profile/{username}")
-    public String showUserProfile(@PathVariable String username, Model model) {
+    public String showUserProfile(@PathVariable String username,
+                                   @AuthenticationPrincipal UserDetailsImpl currentUser,
+                                   Model model) {
         model.addAttribute("profile", profileService.getProfile(username));
+        model.addAttribute("isOwnProfile", currentUser != null && currentUser.getUsername().equals(username));
         return "profile";
     }
 }
